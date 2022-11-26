ee57430a739fb4af876a0900f777612b9736ebe9
Read Langworthy <rlangworthy@gmail.com>
2022-11-25 21:20:07 -0800

    App and store.js changes

--- client/src/store.js
+++ client/src/store.js
@@ -1,26 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from './slices/userSlice'
-import tracksReducer from './slices/tracksSlice'
-import actionsReducer from './slices/actionsSlice'
-import groupsReducer from './slices/groupsSlice'
-import messagesReducer from "./slices/messagesSlice"
-import membersReducer from "./slices/membersSlice"
-import goalsReducer from "./slices/goalsSlice"
-import relationshipsReducer from "./slices/relationshipsSlice"
 import errorsReducer from "./slices/errorsSlice"
 
+// import tracksReducer from './slices/tracksSlice'
+// import actionsReducer from './slices/actionsSlice'
+// import groupsReducer from './slices/groupsSlice'
+// import messagesReducer from "./slices/messagesSlice"
+// import membersReducer from "./slices/membersSlice"
+// import goalsReducer from "./slices/goalsSlice"
+// import relationshipsReducer from "./slices/relationshipsSlice"
 
 export default configureStore({
     reducer: {
         user: userReducer,
-        tracks: tracksReducer, 
-        actions: actionsReducer,
-        groups: groupsReducer,
-        messages: messagesReducer,
-        members: membersReducer,
-        goals: goalsReducer,
-        relationships: relationshipsReducer,
         errors: errorsReducer,
+
+        // tracks: tracksReducer, 
+        // actions: actionsReducer,
+        // groups: groupsReducer,
+        // messages: messagesReducer,
+        // members: membersReducer,
+        // goals: goalsReducer,
+        // relationships: relationshipsReducer,
     },
 })
