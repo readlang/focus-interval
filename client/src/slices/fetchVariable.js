77f44c396ddb65a6b56b1c70cfbdd590336f497d
Read Langworthy <rlangworthy@gmail.com>
2023-01-09 11:29:23 -0800

    add netlify toml file for proxy

--- client/src/slices/fetchVariable.js
+++ client/src/slices/fetchVariable.js
@@ -1,8 +1,10 @@
 // variable to set API fetch path
 
 // use this for development
-// export const path = ""
+//export const path = ""
 
 // use this for deployed production
-export const path = "https://focus-interval.fly.dev"
+// export const path = "https://focus-interval.fly.dev"
 
+// use this for Netlify deployed production
+export const path = "api"
